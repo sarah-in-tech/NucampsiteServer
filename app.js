a08ae4c6c11322ebdaecd9891d1a1cb2a56a14b1
sarah-in-tech <sltadesse25@gmail.com>
2023-03-12 18:31:03 -0500

    Fix Previous

--- app.js
+++ app.js
@@ -47,21 +47,21 @@ app.use(session({
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 function auth(req, res, next) {
-  console.log(req.session);
+    console.log(req.session);
 
-  if (!req.session.user) {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-  } else {
-      if (req.session.user === 'authenticated') {
-          return next();
-      } else {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
-      }
-  }
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } else {
+        if (req.session.user === 'authenticated') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
 }
 
 
